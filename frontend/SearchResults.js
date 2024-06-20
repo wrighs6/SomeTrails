3e44acffb1624e0da2aa23fe18c35dbb7e26568f
CoryTsang <166242425+CoryTsang@users.noreply.github.com>
2024-06-20 14:24:52 -0400

    Update SearchResults.js
    Added images for each trail

--- frontend/SearchResults.js
+++ frontend/SearchResults.js
@@ -5,14 +5,34 @@ export default class SearchResults extends Component {
   render() {
     let results = [`Result for "${this.props.query}"`];
 
-    
+
     if (this.props.query.toLowerCase() === "trail") {
       results.push(
-        { title: "Trail1", description: "This is a description for Trail1." },
-        { title: "Trail2", description: "This is a description for Trail2." },
-        { title: "Trail3", description: "This is a description for Trail3." },
-        { title: "Trail4", description: "This is a description for Trail4." },
-        { title: "Trail5", description: "This is a description for Trail5." }
+        { 
+          title: "Trail1", 
+          description: "This is a description for Trail1.", 
+          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain" 
+        },
+        { 
+          title: "Trail2", 
+          description: "This is a description for Trail2.", 
+          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain" 
+        },
+        { 
+          title: "Trail3", 
+          description: "This is a description for Trail3.", 
+          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain" 
+        },
+        { 
+          title: "Trail4", 
+          description: "This is a description for Trail4.", 
+          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain" 
+        },
+        { 
+          title: "Trail5", 
+          description: "This is a description for Trail5.", 
+          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain" 
+        }
       );
     }
 
@@ -21,8 +41,11 @@ export default class SearchResults extends Component {
         <div class="main-result">${results[0]}</div>
         ${results.slice(1).map(result => html`
           <div class="result-item">
-            <div class="result-title">${result.title}</div>
-            <div class="result-description">${result.description}</div>
+            <img class="result-image" src=${result.imageUrl} alt=${result.title} />
+            <div class="result-content">
+              <div class="result-title">${result.title}</div>
+              <div class="result-description">${result.description}</div>
+            </div>
           </div>
         `)}
       </div>
