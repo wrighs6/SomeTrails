1414d79fab0f7e093233c5dfeaa72fb4c6480b7c
CoryTsang <166242425+CoryTsang@users.noreply.github.com>
2024-06-20 15:04:31 -0400

    Update SearchResults.js
    Added details to be added once api is implemented

--- frontend/SearchResults.js
+++ frontend/SearchResults.js
@@ -11,27 +11,52 @@ export default class SearchResults extends Component {
         { 
           title: "Trail1", 
           description: "This is a description for Trail1.", 
-          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain" 
+          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain",
+          location: "USE API FOR THIS",
+          difficulty: 0,
+          distance: 1.0,
+          elevationGain: 100,
+          time: 60,
         },
         { 
           title: "Trail2", 
           description: "This is a description for Trail2.", 
-          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain" 
+          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain",
+          location: "USE API FOR THIS",
+          difficulty: 0,
+          distance: 1.0,
+          elevationGain: 100,
+          time: 60,
         },
         { 
           title: "Trail3", 
           description: "This is a description for Trail3.", 
-          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain" 
+          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain",
+          location: "USE API FOR THIS",
+          difficulty: 0,
+          distance: 1.0,
+          elevationGain: 100,
+          time: 60,
         },
         { 
           title: "Trail4", 
           description: "This is a description for Trail4.", 
-          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain" 
+          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain",
+          location: "USE API FOR THIS",
+          difficulty: 0,
+          distance: 1.0,
+          elevationGain: 100,
+          time: 60,
         },
         { 
           title: "Trail5", 
           description: "This is a description for Trail5.", 
-          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain" 
+          imageUrl: "https://th.bing.com/th/id/OIP.OnDSYYB09LMRdQfG3GtkgAHaGB?rs=1&pid=ImgDetMain",
+          location: "USE API FOR THIS",
+          difficulty: 0,
+          distance: 1.0,
+          elevationGain: 100,
+          time: 60,
         }
       );
     }
@@ -44,6 +69,11 @@ export default class SearchResults extends Component {
             <div class="result-content">
               <div class="result-title">${result.title}</div>
               <div class="result-description">${result.description}</div>
+              <div class="result-part">${result.location}</div>
+              <div class="result-part">${result.difficulty}</div>
+              <div class="result-part">${result.distance}</div>
+              <div class="result-part">${result.elevationGain}</div>
+              <div class="result-part">${result.time}</div>
             </div>
             <img class="result-image" src=${result.imageUrl} alt=${result.title} />
           </div>
