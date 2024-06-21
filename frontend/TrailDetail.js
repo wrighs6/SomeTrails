73e77881220825125e0a09729531e98377f6466c
Stephen Wright <wrighs6@rpi.edu>
2024-06-21 12:41:45 -0400

    basic trail detail page

--- frontend/TrailDetail.js
+++ frontend/TrailDetail.js
@@ -3,6 +3,21 @@ import { html } from 'htm/preact';
 
 export default class TrailDetail extends Component {
   render() {
-    return html`<p>Trail name: ${this.props.selected}</p>`;
+    console.log(`displaying trail ${this.props.selected}`);
+
+    return html`<div class="trail-detail">
+      <div class="detail-card">
+        <h1>Trail Name Here</h1>
+        <p><a href="">Location Here</a></p>
+        <div class="detail-stats">
+          <span>Difficulty: Placeholder</span>
+          <span>Length: Placeholder</span>
+          <span>Time to complete: Placeholder</span>
+          <span>Elevation gain: Placeholder</span>
+          <span>Highest elevation: Placeholder</span>
+        </div>
+        <p>Description will go here</p>
+      </div>
+    </div>`;
   }
 }
