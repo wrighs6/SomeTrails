8910fc23fefa8b40a4b5c4749d82c8417ae1daa4
Stephen Wright <wrighs6@rpi.edu>
2024-08-04 16:31:10 -0400

    center map

--- frontend/Map.js
+++ frontend/Map.js
@@ -5,7 +5,23 @@ export default class Map extends Component {
   componentDidMount() {
     const path = this.props.path;
 
-    const map = L.map('map').setView([path[0][1], path[0][0]], 14);
+    let minlon = path[0][0];
+    let maxlon = path[0][0];
+    let minlat = path[0][1];
+    let maxlat = path[0][1];
+
+    for (const c of path) {
+      if (c[0] < minlon)
+        minlon = c[0];
+      if (c[0] > maxlon)
+        maxlon = c[0];
+      if (c[1] < minlat)
+        minlat = c[1];
+      if (c[1] > maxlat)
+        maxlat = c[1];
+    }
+
+    const map = L.map('map').setView([(maxlat + minlat) / 2, (maxlon + minlon) / 2], 14);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
